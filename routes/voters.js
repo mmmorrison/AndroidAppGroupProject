52a2095a9ad13696edb49efc643c2ddf0d5c1d8e
Maria <mmorrison1978@gmail.com>
2016-03-17 13:00:43 -0600

    fixed voting json

--- routes/voters.js
+++ routes/voters.js
@@ -16,43 +16,51 @@ function PhotoSet() {
 
 
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
-  Users().select().then(function(results) {
+  PhotoSet().select().then(function(results) {
     res.json(results);
   })
 });
 
-router.get('/decisions', function(req, res, next) {
-  User().where({id: req.param})
-    Decisions().select().then(function(decisions) {
-      res.json(decisions)
-    })
-});
-
-router.get('/photoSet', function(req, res, next) {
-    PhotoSet().select().then(function(photoSet) {
-      res.json(photoSet)
-    })
-});
-
-
-router.get('/:id', function(req, res, next) {
-  Users().where({id: req.params.id}).first().then(function(users) {
-    Decisions().where('user_id', users.id).then(function(decisions) {
-      PhotoSet().where('decision_id', decisions.id).then(function(photos) {
-        res.json(photos)
-      })
-    })
-  })
-});
+// router.get('/decisions', function(req, res, next) {
+//   Users().where({id: req.params.id}).then(function (users) {
+//     Decisions().select().then(function(decisions) {
+//       res.json({"decisions":decisions})
+//     })
+//   })
+// });
+//
+// router.get('/photoSet', function(req, res, next) {
+//     PhotoSet().select().then(function(photoSet) {
+//       res.json(photoSet)
+//     })
+// });
+//
+//
+// router.get('/:id', function(req, res, next) {
+//   var allPhotos = [];
+//   Users().where({id: req.params.id}).first().then(function(users) {
+//     Decisions().where('user_id', users.id).then(function(decisions) {
+//       decisions.forEach(function(decision) {
+//         PhotoSet().where('decision_id', decision.id).then(function(photos, index) {
+//           allPhotos.push(photos)
+//           if (index = photos.length-1) {
+//             res.json(photos);
+//           }
+//         })
+//       })
+//     })
+//   })
+// });
 
 router.post('/', function(req, res, next) {
-  Decisions().insert(req.body).then(function() {
-    res.json({success: true
-    })
+  PhotoSet().update(req.body).then(function(votes, err) {
+    console.log('err = ', err);
+    res.json({success: true});
   })
 })
+
+
 
 
 
