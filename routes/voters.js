00fd82f500b1048e7e04484e967e6fb0c4b749fb
Maria <mmorrison1978@gmail.com>
2016-03-17 14:26:43 -0600

    minor changes

--- routes/voters.js
+++ routes/voters.js
@@ -17,43 +17,16 @@ function PhotoSet() {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('get route');
   PhotoSet().select().then(function(results) {
     res.json(results);
   })
 });
 
-// router.get('/decisions', function(req, res, next) {
-//   Users().where({id: req.params.id}).then(function (users) {
-//     Decisions().select().then(function(decisions) {
-//       res.json({"decisions":decisions})
-//     })
-//   })
-// });
-//
-// router.get('/photoSet', function(req, res, next) {
-//     PhotoSet().select().then(function(photoSet) {
-//       res.json(photoSet)
-//     })
-// });
-//
-//
-// router.get('/:id', function(req, res, next) {
-//   var allPhotos = [];
-//   Users().where({id: req.params.id}).first().then(function(users) {
-//     Decisions().where('user_id', users.id).then(function(decisions) {
-//       decisions.forEach(function(decision) {
-//         PhotoSet().where('decision_id', decision.id).then(function(photos, index) {
-//           allPhotos.push(photos)
-//           if (index = photos.length-1) {
-//             res.json(photos);
-//           }
-//         })
-//       })
-//     })
-//   })
-// });
+
 
 router.post('/', function(req, res, next) {
+  console.log('*********req.body: ', req.body);
   PhotoSet().update(req.body).then(function(votes, err) {
     console.log('err = ', err);
     res.json({success: true});
