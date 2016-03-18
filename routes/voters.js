2e9500314d96793741d6a5b77f84578c509b30c9
Maria <mmorrison1978@gmail.com>
2016-03-18 12:08:51 -0600

    removed logs

--- routes/voters.js
+++ routes/voters.js
@@ -22,15 +22,9 @@ router.get('/', function(req, res, next) {
   })
 });
 
-
-
 router.post('/', function(req, res, next) {
-  console.log('*********req.body: ', req.body);
-  console.log('******* req.body.votes', req.body.votes);
   PhotoSet().insert(req.body).then(function(votes, err) {
-    console.log('err = ', err);
     res.json({success: true});
-    console.log('res******', res);
   })
 })
 
