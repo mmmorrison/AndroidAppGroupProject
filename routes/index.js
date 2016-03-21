73f4a7e60717b942a91bdb36ea3b10010cccc6c6
Maria <mmorrison1978@gmail.com>
2016-03-21 02:35:29 -0600

    changed pics to ones that are hosted on aws

--- routes/index.js
+++ routes/index.js
@@ -29,8 +29,20 @@ router.get('/new', function(req, res, next) {
   })
 });
 
+router.get('/edit/:id', function(req, res, next) {
+  Decisions().select().then(function(results) {
+    res.json(results);
+  })
+});
+
 router.post('/new', function (req, res, next) {
   Decisions().insert(req.body).then(function(photos) {
+    res.json(photos)
+  })
+})
+
+router.post('/edit/:id', function (req, res, next) {
+  Decisions().where('id', req.params.id).first().update(req.body).then(function(photos) {
     console.log("*******************");
     console.log(req.body);
     console.log("*******************");
