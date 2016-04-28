4186e0ec04b4f4109416d616b9b83a116baf5168
Maria <mmorrison1978@gmail.com>
2016-04-28 15:10:10 -0600

    fixed password hash

--- routes/users.js
+++ routes/users.js
@@ -21,12 +21,16 @@ router.get('/register/:name/:password', function(req,res,next) {
   // If the password is correct, return the id,
   // otherwise return a failure status.
   Users().where('name', req.params.name).returning('id','password').then(function(results) {
+    // console.log('results of users query of id and password: err' , results);
     if (results.length != 0) {
       bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(req.params.password, salt, function(err, hash) {
-          bcrypt.compare(hash, results.password, function(err) {
+          console.log('hash: ', hash);
+          console.log('password: ', results[0].password);
+          bcrypt.compare(hash, results[0].password, function(err) {
             if (!err) {
               res.sendStatus(results[0].id.toString());
+              console.log("*****************", results[0].id.toString());
             } else {
               res.sendStatus("-1"); // wrong password
             }
@@ -43,7 +47,8 @@ router.get('/register/:name/:password', function(req,res,next) {
                        res.sendStatus(results[0].id.toString())
                      })
                      .catch(function(error) {
-                       res.sendStatus("-2");//user doesn't exists
+                      //  console.log('err: ', error);
+                       res.sendStatus("-1");
                      })
 
        })
