ce788d2615b76e3158e35f32c531c1123460373d
Aubrey Ford <musicaubrey@gmail.com>
2016-05-01 19:42:49 -0600

    problem with database call

--- routes/users.js
+++ routes/users.js
@@ -17,26 +17,27 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/register', function(req,res,next) {
-  console.log("***********register initiated");
+  console.log("***********register");
 
   Users().where('name', req.body.name).returning('id','password').then(function(results) {
-    console.log("***********", typeof req.body);
-    console.log("***********", results);
+    console.log("***********body", req.body);
+    console.log("***********results", results);
     if (results.length != 0) {
-      console.log("********results.length != 0");
+
       bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(req.body.password, salt, function(err, hash) {
           bcrypt.compare(hash, results[0].password, function(err) {
             if (!err) {
               res.sendStatus(results[0].id.toString());
             } else {
-              res.sendStatus("-1"); // wrong password
+              res.sendStatus("-1");
             }
           })
         })
       })
+
     } else {
-      console.log("*********else");
+
       bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash(req.params.password, salt, function(err, hash) {
         Users().insert({name: req.params.name,
@@ -47,6 +48,7 @@ router.post('/register', function(req,res,next) {
             })
          })
       })
+
     }
   })
 })
