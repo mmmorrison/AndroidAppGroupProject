2faf96e0c5497a1e7d5a7da3b6770f509f1168c3
Aubrey Ford <musicaubrey@gmail.com>
2016-05-01 19:29:08 -0600

    debug

--- routes/users.js
+++ routes/users.js
@@ -19,35 +19,35 @@ router.get('/', function(req, res, next) {
 router.post('/register', function(req,res,next) {
   console.log("***********register initiated");
 
-  // Users().where('name', req.body.name).returning('id','password').then(function(results) {
-  //   console.log("***********", req.body);
-  //   if (results.length != 0) {
-  //     console.log("********results.length != 0");
-  //     bcrypt.genSalt(10, function(err, salt) {
-  //       bcrypt.hash(req.body.password, salt, function(err, hash) {
-  //         bcrypt.compare(hash, results[0].password, function(err) {
-  //           if (!err) {
-  //             res.sendStatus(results[0].id.toString());
-  //           } else {
-  //             res.sendStatus("-1"); // wrong password
-  //           }
-  //         })
-  //       })
-  //     })
-  //   } else {
-  //     console.log("*********else");
-  //     bcrypt.genSalt(10, function(err, salt) {
-  //       bcrypt.hash(req.params.password, salt, function(err, hash) {
-  //       Users().insert({name: req.params.name,
-  //                    password: hash}).returning('id').then(function(results) {
-  //                      res.sendStatus(results[0].id.toString())
-  //                    })
-  //                    .catch(function(error) {
-  //           })
-  //        })
-  //     })
-  //   }
-  // })
+  Users().where('name', req.body.name).returning('id','password').then(function(results) {
+    console.log("***********", req.body);
+    if (results.length != 0) {
+      console.log("********results.length != 0");
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+          bcrypt.compare(hash, results[0].password, function(err) {
+            if (!err) {
+              res.sendStatus(results[0].id.toString());
+            } else {
+              res.sendStatus("-1"); // wrong password
+            }
+          })
+        })
+      })
+    } else {
+      console.log("*********else");
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.params.password, salt, function(err, hash) {
+        Users().insert({name: req.params.name,
+                     password: hash}).returning('id').then(function(results) {
+                       res.sendStatus(results[0].id.toString())
+                     })
+                     .catch(function(error) {
+            })
+         })
+      })
+    }
+  })
 })
 
 
