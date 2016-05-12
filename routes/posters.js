41dba0ec1f545e3e55508f57037898c35151cbdc
Aubrey Ford <musicaubrey@gmail.com>
2016-05-11 22:44:15 -0600

    fixed most server issues

--- routes/posters.js
+++ routes/posters.js
@@ -63,6 +63,7 @@ router.get('/:id', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
+  console.log("router.post /");
   Decisions().insert(req.body).then(function() {
     res.json({success: true
     })
